f2ea8d38457b7836595079c7d145e38ec4f4560a
RuofengX <i@ruofengx.cn>
2024-12-10 21:13:56 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,3 @@
-#![feature(async_iterator)]
-#![feature(async_closure)]
-#![feature(portable_simd)]
-
-pub mod cell;
-pub mod player;
-pub mod pool;
-pub mod pos;
-mod pyhsic;
-
-pub mod prelude {
-    pub use crate::cell::Cell;
-    pub use crate::player::Player;
-    pub use crate::pool::{Pool, Pooling};
-    pub use crate::pos::{Position, Unit};
-}
-
-pub struct World {}
-
 fn main() {
     println!("Hello, world!");
 }
